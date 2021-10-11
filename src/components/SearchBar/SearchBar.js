bca7d1d5df37fc603e96555b026607f012d67053
joshmarv <abujoshua77@gmail.com>
2021-10-11 23:03:06 +0100

    Edited searchbar input field and place holder

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,10 +1,20 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./SearchBar.css";
 
-function SearchBar() {
+function SearchBar(props) {
+	const [search, setSearch] = useState("");
+
+	const handleChange = (e) => {
+		setSearch(e.target.value);
+	};
+	useEffect(() => {}, [search]);
 	return (
 		<div className="search-bar">
-			<input type="text" />
+			<input
+				onChange={(e) => handleChange(e)}
+				placeholder="Search by Name"
+				type="text"
+			/>
 		</div>
 	);
 }
