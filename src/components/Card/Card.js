cd89a6f82f287e04a5e5e9ae698b245eae291fa0
joshmarv <abujoshua77@gmail.com>
2021-10-11 19:53:28 +0100

    Added expandable button and styled it

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,18 +1,27 @@
+import { useState } from "react";
 import "./Card.css";
 const Card = ({ hero }) => {
+	const [toggleCard, setToggleCard] = useState(false);
+	const handleToggleCard = () => {
+		setToggleCard(!toggleCard);
+	};
 	return (
-		<div className="card">
-			<div  className="expand-icon">+
+		<div className="card card-expanded">
+			<div className="expand-icon" onClick={handleToggleCard}>
+				+
 			</div>
 			<div className="image">
 				<img src={hero.images.sm} alt={`hero-${hero.name}`} />
 			</div>
 			<div>
-				<h3>{hero.name}</h3>
-				<p> Full name: {hero.biography.fullName} </p>
-				<p>Race: {hero.appearance.race} </p>
-				<p> Alignment: {hero.biography.alignment} </p>
-				<p>Publisher: {hero.biography.publisher} </p>
+				<div>
+					<h3>{hero.name}</h3>
+					<p> Full name: {hero.biography.fullName} </p>
+					<p>Race: {hero.appearance.race} </p>
+					<p> Alignment: {hero.biography.alignment} </p>
+					<p>Publisher: {hero.biography.publisher} </p>
+				</div>
+				<div className=""></div>
 			</div>
 		</div>
 	);
