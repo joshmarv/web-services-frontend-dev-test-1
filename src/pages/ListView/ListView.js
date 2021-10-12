62a530e2fe46b32b0128b428ea1cd8625ed9fadb
joshmarv <abujoshua77@gmail.com>
2021-10-12 05:16:25 +0100

    Implemented complex algorithm and logic for fetching tags, rendering tags

--- src/pages/ListView/ListView.js
+++ src/pages/ListView/ListView.js
@@ -8,18 +8,78 @@ const ListView = () => {
 	const [data, setData] = useState([]);
 	const [render, setRender] = useState([]);
 	const [query, setQuery] = useState("");
+	const [tags, setTags] = useState({});
+	const [tagQuery, setTagQuery] = useState("");
+	const [tagList, setTagList] = useState([]);
 
-	const findHero = () => {
-		var PATTERN = query;
-		const filtered = data.filter(function (str) {
-			return str.indexOf(PATTERN) === -1;
+	const getTags = () => {
+		let tagList = [];
+		Object.values(tags).forEach((tag) => {
+			tag.forEach((tagItem) => {
+				tagList.append(tagItem.toLowerCase());
+			});
 		});
-		setRender(filtered);
+		const tagListSet = [...new Set(tagList)];
+
+		return tagListSet;
 	};
+	useEffect(() => {
+		const getTags = () => {
+			let tagList = [];
+			Object.values(tags).forEach((tag) => {
+				tag.forEach((tagItem) => {
+					tagList.append(tagItem.toLowerCase());
+				});
+			});
+			const tagListSet = [...new Set(tagList)];
+
+			return tagListSet;
+		};
+		const tagList = getTags();
+		setTagList(tagList);
+	}, [tags]);
 
 	useEffect(() => {
-		findHero();
-	}, [query]);
+		const filterTags = () => {
+			var PATTERN = tagQuery.toLowerCase();
+
+			let keys = [];
+			// Object.values(tags).map(tag => {
+
+			// })
+			Object.entries(tags).forEach(([key, value]) => {
+				value.forEach((tagItem) => {
+					if (tagItem.indexOf(PATTERN) > -1) {
+						keys.push(key);
+						// break;
+					}
+				});
+			});
+			console.log(keys);
+
+			const tagIds = [...new Set(keys)];
+			const filtered = data.filter((word) => !tagIds.includes(word.id));
+			setRender(filtered);
+		};
+
+		filterTags();
+	}, [tagQuery, data, tags]);
+	console.log("tagQuery", tagQuery);
+	console.log(tags);
+	// filter heroes by search
+	useEffect(() => {
+		const findHero = () => {
+			var PATTERN = query.toLowerCase();
+			const filtered = data.filter(
+				(word) => word.name.toLowerCase().indexOf(PATTERN) > -1
+			);
+			setRender(filtered);
+		};
+
+		if (query !== 0) {
+			findHero();
+		}
+	}, [query, data]);
 	useEffect(() => {
 		const fetchData = async () => {
 			// const res = await fetchHeroes();
@@ -41,8 +101,29 @@ const ListView = () => {
 	return (
 		<div className="list-view">
 			<SearchBar query={query} setQuery={setQuery} />
+			{Object.keys(tags).length !== 0 && (
+				<div className="tags">
+					{tagList.map((name) => {
+						return (
+							<span
+								onClick={() => setTagQuery(name)}
+								className="tag"
+							>
+								{name}
+							</span>
+						);
+					})}
+				</div>
+			)}
 			{render.map((item) => {
-				return <Card key={item.id} hero={item} />;
+				return (
+					<Card
+						key={item.id}
+						hero={item}
+						tags={tags}
+						setTags={setTags}
+					/>
+				);
 			})}
 		</div>
 	);
