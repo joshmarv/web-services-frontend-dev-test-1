ab55f8c01e167746da1551836d14e7716f193480
joshmarv <abujoshua77@gmail.com>
2021-10-12 05:17:26 +0100

    Created logic for adding tags to individual cards

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -1,9 +1,39 @@
 import { useState } from "react";
 import "./Card.css";
-const Card = ({ hero }) => {
+const Card = ({ hero, tags, setTags }) => {
+	const [tag, setTag] = useState("");
+
+	const tagId = hero.id;
+	const tagsArray = tags[hero.id];
+	// const tagsArray = ["a", "b"];
+	// console.log("tags", tags);
+	// console.log("tags Array", tagsArray);
+	// if (tags[hero.id] !== undefined) {
+	// 	tagsArray = true;
+	// }
+
 	const [toggleCard, setToggleCard] = useState(false);
 	const handleToggleCard = () => {
 		setToggleCard(!toggleCard);
+	};
+	const onTagChange = (e) => {
+		setTag(e.target.value);
+	};
+	const handleAddTag = () => {
+		if (tagsArray) {
+			const newTagList = [...tagsArray];
+			newTagList.push(tag);
+			let newObj = {};
+			newObj[tagId] = newTagList;
+			setTags({ ...tags, ...newObj });
+		} else {
+			let newObj = {};
+			const newTagList = [];
+			newTagList.push(tag);
+			newObj[tagId] = newTagList;
+			setTags({ ...tags, ...newObj });
+		}
+		setTag(""); // clear the input field
 	};
 	return (
 		<div className={toggleCard ? "card card-expanded" : "card"}>
@@ -13,9 +43,11 @@ const Card = ({ hero }) => {
 			<div className="image">
 				<img src={hero.images.md} alt={`hero-${hero.name}`} />
 			</div>
-			<div className="hero-details">
+			<div
+				className={toggleCard ? "hero-details-active" : "hero-details"}
+			>
 				<div>
-					<h3>{hero.name}</h3>
+					<h3 className="hero-name">{hero.name}</h3>
 					<p> Full name: {hero.biography.fullName} </p>
 					<p>Race: {hero.appearance.race} </p>
 					<p> Alignment: {hero.biography.alignment} </p>
@@ -23,14 +55,41 @@ const Card = ({ hero }) => {
 				</div>
 				{/* {toggleCard && ( */}
 				<div className={toggleCard ? "powers-active" : "powers"}>
-					<h3>Powers</h3>
-					{Object.keys(hero.powerstats).map((power) => {
-						return (
-							<p key={hero.id}>
-								{power} : {hero.powerstats[`${power}`]}
-							</p>
-						);
-					})}
+					<div>
+						<h3 className="powers-heading">Powers</h3>
+						{Object.keys(hero.powerstats).map((power, i) => {
+							return (
+								<p key={i}>
+									{power} : {hero.powerstats[`${power}`]}
+								</p>
+							);
+						})}
+					</div>
+					<div className="tags-section">
+						<h3 className="powers-heading">Tags</h3>
+						<div className="tag-input">
+							<input
+								type="text"
+								onChange={(e) => onTagChange(e)}
+								value={tag}
+							/>
+							<span onClick={handleAddTag} className="btn">
+								Add Tag
+							</span>
+						</div>
+						{tagsArray && (
+							<div className="tags">
+								Tags :
+								{tagsArray.map((name, i) => {
+									return (
+										<span key={i} className="tag">
+											{name}
+										</span>
+									);
+								})}
+							</div>
+						)}
+					</div>
 				</div>
 				{/* // )} */}
 			</div>
